71439cbd490ae350277d39a05bc5d16195ac76a9
shivam-51 <shivamhere247@gmail.com>
2020-09-23 20:30:28 +0530

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
     <div id="app">
         <router-view />
+        <Footer />
     </div>
 </template>
+
+<script>
+import Footer from "./components/Footer";
+export default {
+    name: "app",
+    components: {
+        Footer,
+    },
+};
+</script>
 
 <style lang="scss">
 #app {
--- src/components/authentication/Logincard.vue
+++ src/components/authentication/Logincard.vue
@@ -1,8 +1,15 @@
 <template>
-<div id="signup">
-    <body>
-        <h1>This is the Login page</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam aliquid fugiat quo voluptates pariatur exercitationem esse est quam. Fuga obcaecati deserunt aut, quos illo vitae accusamus. Quam itaque architecto illo est iste soluta provident voluptatem obcaecati eligendi fuga dolorum rem magnam unde beatae minima eius, nihil nesciunt fugiat? Temporibus, et!</p>
-    </body>
-</div>
+    <div id="signup">
+        <body>
+            <h1>This is the Login page</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+                aliquid fugiat quo voluptates pariatur exercitationem esse est
+                quam. Fuga obcaecati deserunt aut, quos illo vitae accusamus.
+                Quam itaque architecto illo est iste soluta provident voluptatem
+                obcaecati eligendi fuga dolorum rem magnam unde beatae minima
+                eius, nihil nesciunt fugiat? Temporibus, et!
+            </p>
+        </body>
+    </div>
 </template>
