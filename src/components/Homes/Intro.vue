9476966585e34a4e5c018dd3dea792683df68fc6
praneetha3108 <58747237+praneetha3108@users.noreply.github.com>
2020-09-27 21:46:26 +0530

    completed homepage

--- src/components/Homes/Intro.vue
+++ src/components/Homes/Intro.vue
@@ -3,43 +3,32 @@
     <section id="create-section" class="py-5 mt-5">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 order-2">
+               
+                <div class="col-md-6 mt-3 order-1">
+                    <h3>IT Department-IIEST,shibpur</h3>
+                    <p class="lead">
+                       Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur)  comes under the Council of National Institutes of Technology.2nd oldest institute in India (163 years old) .  The Department of Information Technology started its journey in 2000. The department has produced excellent IT engineers who are serving in different reputed organizations and pursuing higher studies at various Institutes of Excellence.
+                    </p>
+                </div>
+                 <div class="col-md-6 order-2">
                     <img
-                        src="@/assets/ITlogo.png"
+                        src="@/assets/main.png"
                         class="img-fluid mg-3 rounded-circle"
                         alt=""
                     />
                 </div>
-                <div class="col-md-6 mt-3 order-1">
-                    <h3>Create your Passion</h3>
-                    <p class="lead">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. In nihil consequuntur quidem animi quam
-                        necessitatibus deserunt ea suscipit consequatur
-                        quisquam.
-                    </p>
-
-                    <div class="d-flex">
-                        <div class="p-4 align-self-start">
-                            <i class="fas fa-check fa-2x"></i>
-                        </div>
-                        <div class="p-4 align-self-end">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Alias quae odio ratione cupiditate et ipsum
-                            quaerat perspiciatis nihil exercitationem deserunt.
-                        </div>
-                    </div>
-                    <div class="d-flex">
-                        <div class="p-4 align-self-start">
-                            <i class="fas fa-check fa-2x"></i>
-                        </div>
-                        <div class="p-4 align-self-end">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Alias quae odio ratione cupiditate et ipsum
-                            quaerat perspiciatis nihil exercitationem deserunt.
-                        </div>
-                    </div>
+                <div class="col-md-6 order-2">
+                       <img src="@/assets/itlab.jpg"
+                        class="img-fluid mg-3 rounded-circle"
+                        alt=""
+                    />
                 </div>
+                 <div class="col-md-6 mt-3 order-3"> <h3>POST-IT</h3>
+                    <p class="lead">
+                      POST-IT is a social platform to connect people and form a network. Users can share the articles to social media feeds, have endless conversation and authentic human connection. The community can share content by posting stories, links, images, and videos in the form of blogs. This platform is designed by the Department of Information Technology, IIEST Shibpur with an aim to meet new people, share ideas, interest they care about the most, success stories with a wide diversity of interests.
+
+                    </p></div>
+               
             </div>
         </div>
     </section>
@@ -53,3 +42,4 @@ export default {
 
 <style>
 </style>
+
