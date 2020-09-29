4eeb242b3ed0c51e84897c93e5fe34a2603cbea0
Divyansh Masiwal <divyanshm1609@gmail.com>
2020-09-29 11:48:01 +0530

    Added Team page and removed additional files

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,16 @@ const routes = [
     path: '/addblog',
     name: 'AddBlog',
     component: () => import('../views/AddBlog.vue')
-  }
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component: () => import('../views/Team.vue')
+  },
+  
+  
+  
+
 ]
 
 const router = new VueRouter({
