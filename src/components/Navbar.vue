66c873f1909a2dfa4739a7210e7e6b006ab4f8a8
Andikha Dian Nugraha <andikha.dian1@gmail.com>
2020-10-01 10:28:46 +0700

    added margin top in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,58 +1,38 @@
 <template>
-    <div fixed:top class="shadow p-0 bg-white rounded" id="navbar">
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-nav>
-                <!-- Navbar dropdowns -->
-                <b-nav-item-dropdown text="☰" left>
-                    <b-dropdown-item
-                        href="https://www.iiests.ac.in/"
-                        target="_blank"
-                        >IIEST</b-dropdown-item
-                    >
-                    <b-dropdown-item
-                        href="https://www.iiests.ac.in/IIEST/AcaUnitDetails/IT"
-                        target="_blank"
-                        >IT-IIEST</b-dropdown-item
-                    >
-                    <b-dropdown-item
-                        href="https://www.facebook.com/search/top?q=society%20of%20information%20technology%20-%20socit"
-                        target="_blank"
-                        >SOCIT</b-dropdown-item
-                    >
-                </b-nav-item-dropdown>
-            </b-navbar-nav>
-            <b-navbar-brand href="#" to="/">
-                <img
-                    class="icon"
-                    src="../../src/assets/PIlogo.png"
-                    width="39"
-                />
-                {{ title }}
-            </b-navbar-brand>
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  <div fixed:top class="shadow p-0 bg-white rounded" id="navbar">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-nav>
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="☰" left>
+          <b-dropdown-item href="https://www.iiests.ac.in/" target="_blank">IIEST</b-dropdown-item>
+          <b-dropdown-item
+            href="https://www.iiests.ac.in/IIEST/AcaUnitDetails/IT"
+            target="_blank"
+          >IT-IIEST</b-dropdown-item>
+          <b-dropdown-item
+            href="https://www.facebook.com/search/top?q=society%20of%20information%20technology%20-%20socit"
+            target="_blank"
+          >SOCIT</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+      <b-navbar-brand href="#" to="/">
+        <img class="icon" src="../../src/assets/PIlogo.png" width="39" />
+        {{ title }}
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item href="#" to="/addblog">Blogs</b-nav-item>
-                    <b-nav-item href="">Thread</b-nav-item>
-                    <b-nav-item href="#" to="/team">Meet the team</b-nav-item>
-                </b-navbar-nav>
-                <b-collapse id="nav-collapse" is-nav>
-                    <!-- Right aligned nav items -->
-                    <b-navbar-nav class="ml-auto">
-                        <b-button
-                            variant="outline-warning"
-                            class="mr-3"
-                            to="signup"
-                            >Sign Up</b-button
-                        >
-                        <b-button
-                            variant="outline-warning"
-                            class="mr-2"
-                            to="login"
-                            >Log In</b-button
-                        >
-                        <!-- <b-nav-item-dropdown right>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" to="/addblog">Blogs</b-nav-item>
+          <b-nav-item href>Thread</b-nav-item>
+          <b-nav-item href="#" to="/team">Meet the team</b-nav-item>
+        </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-button variant="outline-warning" class="mr-3" to="signup">Sign Up</b-button>
+            <b-button variant="outline-warning" class="mr-2" to="login">Log In</b-button>
+            <!-- <b-nav-item-dropdown right>
                             <template v-slot:button-content>
                                 <display4>User</display4>
                             </template>
@@ -60,28 +40,31 @@
                             <b-dropdown-item href="#" to="/About"
                                 >Sign Out</b-dropdown-item
                             >
-                        </b-nav-item-dropdown> -->
-                    </b-navbar-nav>
-                </b-collapse>
-            </b-collapse>
-        </b-navbar>
-    </div>
+            </b-nav-item-dropdown>-->
+          </b-navbar-nav>
+        </b-collapse>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "navbar",
-    props: { title: String },
+  name: "navbar",
+  props: { title: String }
 };
 </script>
 
 <style scoped>
+#navbar {
+  margin-top: 5px;
+}
 .bg-info {
-    background-color: #1e0c42 !important;
-    padding: 0.7rem;
+  background-color: #1e0c42 !important;
+  padding: 0.7rem;
 }
 .icon {
-    display: inline-block;
-    margin-right: 2px;
+  display: inline-block;
+  margin-right: 2px;
 }
 </style>>
