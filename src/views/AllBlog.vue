699b4af19b99660b39e90f9c2a8cc44ec95a0413
shivam kumar singh <47945055+shivam-51@users.noreply.github.com>
2020-10-04 18:55:19 +0530

    Blog page added + major changes (#65)
    * random changes done
    
    * Major changes done

---
+++ src/views/AllBlog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="component display-4">
+    <p>All the blogs will be shown here.</p>
+    <p>
+      <router-link to="/addblog">Click here</router-link> to add a new blog.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "allblog"
+};
+</script>
+
+<style scoped>
+.component {
+  margin-top: 30vh;
+  margin-bottom: 30vh;
+}
+</style>
