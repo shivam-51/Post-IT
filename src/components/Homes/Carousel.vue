109cc306dcb0823fb55280fb9f49c5cdc19876e7
praneetha3108 <58747237+praneetha3108@users.noreply.github.com>
2020-10-23 14:40:48 +0530

    added a social sticky bar in home page

--- src/components/Homes/Carousel.vue
+++ src/components/Homes/Carousel.vue
@@ -1,5 +1,13 @@
 <template>
+
   <div>
+    <div class="icon-bar">
+ <a href="https://www.facebook.com/iiest.shibpur.official" class="facebook"><i class="fab fa-facebook-f"></i></a> 
+  <a href="https://twitter.com/iiest_s" class="twitter"><i class="fab fa-twitter"></i></a> 
+  <a href="https://iiests.irins.org/" class="vidwan"><i class="fas fa-lightbulb"></i></a> 
+  <a href="https://www.linkedin.com/school/iiests/" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+  <a href="shivamhere247@gmail.com" class="gmail"><i class="  fas fa-envelope"></i></a> 
+</div>
     <b-carousel
       id="carousel"
       v-model="slide"
@@ -62,6 +70,8 @@
 #carousel {
   padding-top: 75px;
   margin-top: 40px;
+  width:90%;
+  left:70px;
   text-align: center;
 }
 .carousel-control-prev-icon,
@@ -91,4 +101,50 @@
 #HR {
   height: 240px;
 }
+.icon-bar {
+  position: fixed;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 16px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 20px;
+}
+
+.icon-bar a:hover {
+  background-color: #000;
+}
+
+.facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.vidwan {
+  background: purple;
+  color: white;
+}
+
+.linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.gmail {
+  background: #bb0000;
+  color: white;
+}
+
 </style>
